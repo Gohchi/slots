a85bb5f874bf7b59c64d74366232ec6a4f06195f
Martín Farias <martin.akg1991@gmail.com>
2023-09-30 17:15:16 -0300

    update project

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,61 @@
 const webpack = require("webpack");
 const path = require("path");
 
+// Phaser webpack config
+const phaserModule = path.join(__dirname, './node_modules/phaser/');
+const phaser = path.join(phaserModule, './src/phaser.js');
+
 module.exports = {
   entry: {
-    app: [path.resolve(__dirname, 'src/main.js')],
-    vendor: ['phaser']
+    app: './src/game.js',
+    vendor: 'phaser'
   },
   output: {
-    pathinfo: true,
+    // pathinfo: true,
     path: path.resolve(__dirname, 'dist'),
     publicPath: './dist/',
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
+  target: 'web',
   module: {
     rules: [
       {
         test: /\.js$/,
         use: ['babel-loader'],
         include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|xml|mp3)$/i,
+        type: 'asset/resource'
       }
     ]
   },
-  optimization: {
-    splitChunks: {
-      name: 'vendor',
-      chunks: 'all'
-    }
-  },
+  // optimization: {
+  //   splitChunks: {
+  //     name: 'vendor',
+  //     chunks: 'all'
+  //   }
+  // },
   plugins: [
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
-    })
-  ]
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ],
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      'phaser': phaser,
+    },
+    fallback: {
+      "buffer": require.resolve("buffer/")
+    }
+  }
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,18 +1,43 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const definePlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
+  WEBGL_RENDERER: true, 
+  CANVAS_RENDERER: true 
+});
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
-  watch: true,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
   plugins: [
-    new BrowserSyncPlugin({
-      host: process.env.IP || 'localhost',
-      port: process.env.PORT || 3000,
-      server: {
-        baseDir: ['./', './build']
-      }
-    })
+    definePlugin,
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.webpack.html',
+      chunks: ['vendor', 'app'],
+      chunksSortMode: 'manual',
+      minify: {
+        removeAttributeQuotes: false,
+        collapseWhitespace: false,
+        html5: false,
+        minifyCSS: false,
+        minifyJS: false,
+        minifyURLs: false,
+        removeComments: false,
+        removeEmptyAttributes: false
+      },
+      hash: false
+    }),
   ]
 })
