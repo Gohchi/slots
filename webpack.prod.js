4aa695022eed419199fb34beaf32cb124e42367f
Martín Farias <martin.akg1991@gmail.com>
2023-09-30 18:32:24 -0300

    update webpack build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,9 @@
 const path = require('path')
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
  
 module.exports = merge(common, {
@@ -13,23 +13,23 @@ module.exports = merge(common, {
     pathinfo: true,
     path: path.resolve(__dirname, 'dist'),
     publicPath: './',
-    filename: 'bundle.min.js'
+    filename: 'bundle.[name].min.js'
   },
   performance: {
     maxEntrypointSize: 900000,
     maxAssetSize: 900000
   },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  },
+  // optimization: {
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       terserOptions: {
+  //         output: {
+  //           comments: false
+  //         }
+  //       }
+  //     })
+  //   ]
+  // },
   module: {
     rules: [
       {
@@ -50,22 +50,27 @@ module.exports = merge(common, {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "./")
-    }),
     new HtmlWebpackPlugin({
-      template: "./index.prod.html"
-    }),
-    // new CopyPlugin([
-    //   { from: 'source', to: 'dest' },
-    //   { from: 'other', to: 'public' },
-    // ]),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, './assets'),
-        to: path.resolve(__dirname, './dist/assets'),
-        copyUnmodified: true,
+      filename: 'index.html',
+      template: './src/index.webpack.html',
+      chunks: ['vendor', 'app'],
+      chunksSortMode: 'manual',
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        html5: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        removeComments: true,
+        removeEmptyAttributes: true
       },
-    ])
+      hash: true
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets/favicon.ico', to: 'assets/favicon.ico' }
+      ],
+    })
   ]
 });
